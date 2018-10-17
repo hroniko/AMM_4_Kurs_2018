d6b364090314fc63970b0b9aad3bd75ceba0300e
hroniko <hroniko@bk.ru>
2018-10-17 19:54:14 +0300

    fix lab 3 Lotka-Volterra as solve odu

--- python/lab03/main.py
+++ python/lab03/main.py
@@ -9,9 +9,9 @@ dy/dt =-y*(gamma - delta*x);
 
 def system(z,t):
    x,y=z[0],z[1]
-   dxdt= x*(alpha-beta*y)
+   dxdt= x*(alpha-beta*y) # вычисление правых частей
    dydt=-y*(gamma-delta*x)
-   return [dxdt,dydt]
+   return [dxdt,dydt] # объединяем в список
 
 if __name__ == '__main__':
     import numpy as np
@@ -23,13 +23,26 @@ if __name__ == '__main__':
     gamma = 0.0225
     delta = 0.02
 
-    t = np.linspace(0, 300., 1000)
-    x0, y0 = 1.0, 1.0
-    sol = odeint(system, [x0, y0], t)
-    X, Y = sol[:, 0], sol[:, 1]
-    plt.plot(X, Y, 'b-', linewidth=5)
+    t = np.linspace(0, 300., 1000) # разбиваем на 1000 частей отрезок, список времени
+    x0, y0 = 1.0, 1.0 # начальные условия
+    sol = odeint(system, [x0, y0], t) # передаем функцию для вычисления правых частей, спико нач условий, список временных точек
+    X, Y = sol[:, 0], sol[:, 1] # нулевой и первый столбец значений
+    plt.plot(X, Y, 'b-', linewidth=5) # строим траекторию движения точки в соответсвии с решением
     plt.grid(True)
     plt.xlabel("x")
     plt.ylabel("y")
     plt.savefig('y1.pdf', dpi=300)
+    plt.show()
+
+    fig, (ax0, ax1) = plt.subplots(nrows=2)
+    ax0.plot(t, X, 'b-', linewidth=5)
+    ax0.set_title('solve X(t)')
+    ax0.grid(True)
+
+    ax1.plot(t, Y, 'r-', linewidth=5)
+    ax1.set_title('solve Y(t)')
+    ax1.grid(True)
+
+
+    fig.subplots_adjust(hspace=0.3)
     plt.show()
